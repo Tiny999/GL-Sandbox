e39b79585c34dfd78c18f579dbbcb6b3d4c54c57
Abbas Salami <olawalesalami3@gmail.com>
2023-03-07 01:48:30 +0100

    add glad and glfw

--- GL-Sandbox/Main.cpp
+++ GL-Sandbox/Main.cpp
@@ -1,6 +1,35 @@
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Sandbox", NULL, NULL);
+
+	if (!window)
+	{
+		std::cout << "Failed to initialized window";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+
 	return 0;
 }
